e97e9c8822d02290c11bb785ba2ef88cc51ef338
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-03-18 21:25:39 -0300

    Added Tabelas

--- index.js
+++ index.js
@@ -9,9 +9,87 @@ const fonts = {
   }
 }
 
+/* Primeira Parte: ------------------ */
+// const printer = new PdfPrinter(fonts)
+// const docDefinition = {
+//   content: [
+//     { text: 'Fullstack Master' },
+//     {
+//       table: {
+//         widths: ['*', '*', 100],
+//         body: ['Nome', 'E-mail', 'Situação']
+//       }
+//     }
+//   ]
+// }
+
+
+
+/* Segunda Parte: ------------------ */
+// const lines = []
+// lines.push(['Nome', 'E-mail', 'Situação'])
+// for (let i = 0; i < 300; i++) {
+//   lines.push(['Tulio Faria', 'tuliofaria@devpleno.com', 'Ativo'])
+// }
+
+// const printer = new PdfPrinter(fonts)
+// const docDefinition = {
+//   content: [
+//     { text: 'Fullstack Master' },
+//     {
+//       table: {
+//         widths: ['*', '*', 100],
+//         body: lines
+//       }
+//     }
+//   ]
+// }
+
+/* Terceira Parte: ------------------ */
+const lines = []
+lines.push([
+  {
+    text: 'Nome',
+    style: 'header'
+  },
+  {
+    text: 'E-mail',
+    style: 'header'
+  },
+  {
+    text: 'Situação',
+    style: 'header'
+  },
+])
+for (let i = 0; i < 300; i++) {
+  let ativo = 'Ativo'
+  if (i % 2 === 0) {
+    ativo = { text: 'Inativo', style: 'inativo' }
+  }
+  lines.push(['Tulio Faria', 'tuliofaria@devpleno.com', ativo])
+}
+
 const printer = new PdfPrinter(fonts)
 const docDefinition = {
-  content: 'Fullstack Master'
+  content: [
+    { text: 'Fullstack Master' },
+    {
+      table: {
+        widths: ['*', '*', 100],
+        body: lines
+      }
+    }
+  ],
+  styles: {
+    header: {
+      fontSize: 18,
+      bold: true
+    },
+    inativo: {
+      fontSize: 18,
+      bold: true
+    }
+  }
 }
 
 const pdf = printer.createPdfKitDocument(docDefinition)

25bd350e1e98b5712667783cd35367e4614e48dd
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-03-18 22:44:43 -0300

    Added Express

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const PdfPrinter = require('pdfmake')
-
 const fonts = {
   Roboto: {
     normal: 'fonts/Roboto-Regular.ttf',
@@ -9,76 +8,20 @@ const fonts = {
   }
 }
 
-const lines = []
-lines.push([
-  {
-    text: 'Nome',
-    style: 'header'
-  },
-  {
-    text: 'E-mail',
-    style: 'header'
-  },
-  {
-    text: 'Situação',
-    style: 'header'
-  },
-])
-for (let i = 0; i < 300; i++) {
-  let ativo = 'Ativo'
-  if (i % 2 === 0) {
-    ativo = { text: 'Inativo', style: 'inativo' }
-  }
-  lines.push(['Tulio Faria', 'tuliofaria@devpleno.com', ativo])
-}
+const express = require('express')
+const app = express()
 
 const printer = new PdfPrinter(fonts)
-const docDefinition = {
-  content: [
-    {
-      image: 'images/logo.png',
-      // width: 100,
-      // heigth: 100
-      fit: [80, 100]
-    },
-    { text: 'Fullstack Master' },
-    {
-      table: {
-        widths: ['*', '*', 100],
-        body: lines
-      }
-    }
-  ],
-  styles: {
-    header: {
-      fontSize: 18,
-      bold: true
-    },
-    inativo: {
-      fontSize: 18,
-      bold: true
-    }
-  },
-  footer: (page, pages) => {
-    return {
-      columns: [
-        'Este documento é parte integrante do Fullstack Master',
-        {
-          alignment: 'right',
-          text: [
-            { text: page.toString(), italics: true },
-            ' de ',
-            { text: pages.toString(), italics: true }
-          ]
-        }
-      ],
-      margin: [40, 0]
-    }
-  }
-}
 
-const pdf = printer.createPdfKitDocument(docDefinition)
-const fs = require('fs')
-pdf.pipe(fs.createWriteStream('doc.pdf'))
-pdf.end()
+app.get('/get/:name', (req, res) => {
+  const pdf = printer.createPdfKitDocument({
+    content: 'Olá ' + req.params.name
+  })
+  // res.header('Content-disposition', 'inline; filename=meu-pdf.pdf')
+  res.header('Content-disposition', 'attachment; filename=meu-pdf.pdf')
+  res.header('Content-type', 'application/pdf')
+  pdf.pipe(res)
+  pdf.end()
+})
 
+app.listen(3000, () => console.log('pdf running...'))
